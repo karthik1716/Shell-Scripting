3d35f511f588ee16d4c683ee7c6d6894671c040c
karthik1716 <karthikarumugam73@gmail.com>
2024-06-17 17:05:37 +0530

    exercise-2

---
+++ exercise-2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Hello,Welcome back Karthik...."
+
+date
+
+mkdir test
+
+pwd
+
+cd test
+
+touch sample.txt
+
+echo "This is sample text" > sample.txt
+
+cd ..
+
+echo "Successfully executed the following script. Goodbye Boss...."
